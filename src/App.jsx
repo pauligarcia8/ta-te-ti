1fa1ca9e0ac51bb08914669777d6326e5d5bb322
Paula Garcia <pauligarcia_05@hotmail.com>
2025-03-13 16:53:53 -0300

    add tateti logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,12 @@
+import Board from "./components/Board";
+import Title from "./UI/Title";
+
 function App() {
-
-  const tableboard = [
-    {
-      value: null,
-      border: 'border-b-2 border-r-2'
-    },
-    {
-      value: null,
-      border: 'border-b-2 border-r-2'
-    },
-    {
-      value: null,
-      border: 'border-b-2'
-    },
-    {
-      value: null,
-      border: 'border-b-2 border-r-2'
-    },
-    {
-      value: null,
-      border: 'border-b-2 border-r-2'
-    },
-    {
-      value: null,
-      border: 'border-b-2'
-    },
-    {
-      value: null,
-      border: 'border-r-2'
-    },
-    {
-      value: null,
-      border: 'border-r-2'
-    },
-    {
-      value: null,
-      border: 'border-none'
-    },
-  ]
-
 
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-pink-400">
-      <h1 className="w-full text-lg text-blue-600 font-bold text-center m-4">
-        TA TE TI
-      </h1>
-      <div className="w-[600px] h-[600px] grid grid-cols-3">
-        {tableboard.map((box) => (
-
-        <div className={`w-full h-full border-amber-400 ${box.border}`}></div>
-        ))}
-      </div>
+      <Title>TA TE TI</Title>
+      <Board />
     </div>
   );
 }
---
+++ src/UI/Title.jsx
@@ -0,0 +1,9 @@
+const Title = ({ children }) => {
+  return (
+    <h1 className="w-full text-5xl text-blue-600 font-bold text-center m-4">
+      {children}
+    </h1>
+  );
+};
+
+export default Title;
